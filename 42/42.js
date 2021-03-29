af060184f01be0feb7e9a604fdf7c1a7541f3852
Athena <ytblackfalcon@gmail.com>
2021-03-29 16:04:59 +0200

    Mise à jour calcul 42

--- 42/42.js
+++ 42/42.js
@@ -1,10 +1,14 @@
 function check() {
     var invalid =document.getElementById("input").value;
     var if42 =document.getElementById("input").value;
+    var valid =document.getElementById("input").value;
     if (invalid == ""){
-        alert("Veuillez saisir un nombre")
+        alert("Veuillez saisir un nombre");
     }
     if (if42 == 42){
-        alert ("Désolé, ma programmation m'interdit de calculer avec cette valeur")
+        alert ("Désolé, ma programmation m'interdit de calculer avec cette valeur");
+    }
+    else {
+        alert (valid*=2);
     }
 }
