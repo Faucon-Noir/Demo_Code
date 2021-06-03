adec9baee907577c4d0bf626cf2deeadbe75b96b
Faucon Noir <ytblackfalcon@gmail.com>
2021-06-03 21:11:33 +0200

    add C#

---
+++ C_sharp/Controllers/Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Intro_C_sharp.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class home : Controller
+    {
+        [HttpGet("hello")]
+        public ActionResult hello()
+        {
+            var data = new { Etna = "Hello World" };
+            return Json(data);
+        }
+    }
+}
---
+++ C_sharp/Migrations/20210602114857_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace C_sharp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UsersId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Rôle = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UsersId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
